7b528bdc3858d84b146100dfba5b93c29dd750d1
shannmc <shannonmcnellis@Shannons-MBP.hsd1.sc.comcast.net>
2020-08-23 19:52:04 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { UsersComponent } from './admin/users/users.component';
+import {RouterModule, Routes} from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {path : 'admin/users', component : UsersComponent},
+  {path : 'admin/restaurants', component: RestaurantsComponent},
+  {path : '404', component : PageNotFoundComponent},
+  {path : '**', redirectTo : '/404'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RestaurantsComponent,
+    UsersComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/restaurants/restaurants.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rooms',
+  templateUrl: './restaurants.component.html',
+  styleUrls: ['./restaurants.component.css']
+})
+export class RestaurantsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
